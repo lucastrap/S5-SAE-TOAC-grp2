61e34dc0ee38e782fdca4f561540af5077a4997c
BSCT-Tormod <thomasdemeyere33@gmail.com>
2023-09-20 13:53:05 +0200

    dev

--- code/script.js
+++ code/script.js
@@ -2,9 +2,20 @@ document.querySelectorAll('.section').forEach(section => {
     if (section.querySelector('.section-title') === null) {
         return;
     }
+    section.querySelectorAll('div').forEach(element => {
+        element.addEventListener('mouseover', () => {
+            section.querySelector('.section-title').style.display = 'none';
+            section.querySelector('.section-content').style.display = 'block';
+        });
+    });
     section.addEventListener('mouseover', () => {
-        section.querySelector('.section-title').hidden = true;
-        section.querySelector('.section-elements').hidden = false;
-        console.log(section.querySelector('.section-title').innerHTML);
+        section.querySelector('.section-title').style.height = '60%';
+        section.querySelector('.section-title').style.width = '70%';
+    });
+    section.addEventListener('mouseout', () => {
+        section.querySelector('.section-title').style.height = '50%';
+        section.querySelector('.section-title').style.width = '60%';
+        section.querySelector('.section-title').style.display = 'flex';
+        section.querySelector('.section-content').style.display = 'none';
     });
 });
