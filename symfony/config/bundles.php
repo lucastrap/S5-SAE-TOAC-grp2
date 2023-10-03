8786269ebb5feb4ec40247a0cf8833257fe6ccf2
hugomonte <24875641+hugomonte@users.noreply.github.com>
2023-10-03 15:19:12 +0200

    fix: conflicts

--- symfony/config/bundles.php
+++ symfony/config/bundles.php
@@ -7,8 +7,5 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
-<<<<<<< HEAD
-=======
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
->>>>>>> fb9cbb8c40b41d0ba54b73ccb779ec1d9d258116
 ];
