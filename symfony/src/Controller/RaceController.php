a2fec463d739a9ba86aae07d73cc53306bf3a5f0
Thomas MASIN (SAN DE RGOCC) <56608891+caerroff@users.noreply.github.com>
2023-10-05 16:10:34 +0200

    feat(symfony) Added races to the race template

--- symfony/src/Controller/RaceController.php
+++ symfony/src/Controller/RaceController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Course;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +11,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class RaceController extends AbstractController
 {
     #[Route('/race', name: 'race')]
-    public function index(): Response
+    public function index(EntityManagerInterface $em): Response
     {
+        $triathlon = $em->getRepository(Course::class)->findAll();
         return $this->render('race/index.html.twig', [
             'controller_name' => 'RaceController',
+            'triathlon' => $triathlon
         ]);
     }
 }
