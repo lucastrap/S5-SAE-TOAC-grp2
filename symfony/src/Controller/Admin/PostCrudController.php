b4b740d702dd81b4fb47d966103c18ce6d28d2b7
hugomonte <24875641+hugomonte@users.noreply.github.com>
2023-10-10 10:02:51 +0200

    feat(admin): add id field on show for posts

--- symfony/src/Controller/Admin/PostCrudController.php
+++ symfony/src/Controller/Admin/PostCrudController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Admin;
 
 use App\Entity\Post;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
@@ -17,6 +18,7 @@ class PostCrudController extends AbstractCrudController
     public function configureFields(string $pageName): iterable
     {
         return [
+            IdField::new('id')->hideOnForm(),
             TextField::new('title'),
             TextEditorField::new('content'),
         ];
